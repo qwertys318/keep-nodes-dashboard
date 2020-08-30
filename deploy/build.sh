4e40150963c6ba49d63e41d880fb9ca0b87c2f45
qwertys318 <343604@gmail.com>
2020-08-30 17:54:32 +0700

    prod env, sentry, fixes

---
+++ deploy/build.sh
@@ -0,0 +1,13 @@
+rm -rf build
+mkdir build
+cd ./build
+git clone git@github.com:qwertys318/keep-nodes-dashboard.git
+git checkout "${1}"
+sed -i -e "s/__APP_VERSION__/${1}/" ./.env
+composer install --no-dev --no-interaction --no-cache
+composer dump-autoload -o
+composer dump-env prod
+rm -rf ./.git ./.env ./front
+docker build -f ../deploy/Dockerfile -t docker.pkg.github.com/qwertys318/keep-nodes-dashboard/fpm:${1} -t docker.pkg.github.com/qwertys318/keep-nodes-dashboard/fpm:latest  .
+docker push docker.pkg.github.com/qwertys318/keep-nodes-dashboard/fpm:${1}
+docker push docker.pkg.github.com/qwertys318/keep-nodes-dashboard/fpm:latest
--- src/Command/AbstractCommand.php
+++ src/Command/AbstractCommand.php
@@ -10,21 +10,25 @@ use Symfony\Component\Lock\Store\FlockStore;
 
 abstract class AbstractCommand extends Command
 {
-    /** @var Logger */
-    protected $logger;
-    /** @var EntityManagerInterface */
-    protected $em;
+    protected Logger $logger;
+    protected EntityManagerInterface $em;
+    protected string $projectDir;
 
     public function setEm(EntityManagerInterface $em): void
     {
         $this->em = $em;
     }
 
+    public function setProjectDir(string $projectDir): void
+    {
+        $this->projectDir = $projectDir;
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->logger->info("Start command: {$this->getName()}");
 
-        $store = new FlockStore();
+        $store = new FlockStore(sprintf('%s/var/db', $this->projectDir));
         $factory = new LockFactory($store);
         $lock = $factory->createLock(md5(serialize($input->getArguments())));
 
